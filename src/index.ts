9007933f20b4471399ef16b0920806d4360a21d2
Windy Feng <1745500559@qq.com>
2021-07-12 16:13:00 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export * as PDKAbstractDataTypes from './AbstractDataTypes/ALL';
 export * as PDKInternalDataTypes from './InternalDataTypes/ALL';
-export * as PDKutils from './Utilities/ALL';
+export * as PDKUtils from './Utilities/ALL';
